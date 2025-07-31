ccb98f12d9b6aa5efe21d6c4518dcd049eb75c7e
Mohamad Ashif Istawa <aashifalam727@gmail.com>
2025-08-01 00:34:11 +0530

    Update Personal_Budget_Tracker.py
    finish console based my personal budget tracker with all the function in python

--- Personal_Budget_Tracker.py
+++ Personal_Budget_Tracker.py
@@ -1,13 +1,48 @@
-def addincome():
+import datetime as dt 
+total_income = 0
+
+def add_Income():
     income=int(input("enter the addincome: "))
     total_income=income
     return total_income 
 
 
+def expense_record():
+    global total_income
+    income=0
+    expense=0
+    while(True):
+        date = input("Please enter the date (YYYY-MM-DD): ")
+        try:
+            date_obj = dt.datetime.strptime(date, "%Y-%m-%d").date()
+        except ValueError:
+            print("Invalid date format. Please enter date as YYYY-MM-DD.")
+            break
+        type = input("Enter the mode of amount expense/Income : ")
+        if(type.lower() == "expense"):                
+            expense = input("Enter Your expense amount: ")
+        elif(type.lower() == "income"):     
+            income = input("Enter Your income amount: ")
+        else:
+            print("Enter Right option")
+            break  
+        description = input("Enter the description of the amount: ")
+        record_list = date + " | " + type + " | " + (expense or income) + " | " + description + "\n"
+        total_income = total_income-int(expense)
+        total_income = total_income+int(income)
+        with open("Expense_record.txt","a") as f:
+            f.write(record_list)
+        break
+    return total_income
+
+def record():
+    with open("Expense_record.txt") as f:
+        print("...............Your Transaction history/.........................")
+        print(f.read())
 
 
-
-total_income=0
+income = 0
+expense = 0
 def show_menu():
     while(True):
         print("1.Add Income")
@@ -17,9 +52,15 @@ def show_menu():
         print("5.Exit")
         user=int(input("enter option from the menu: "))
         if(user==1):
-            total_income = addincome()
+            global total_income
+            total_income = add_Income()
+        elif(user == 2):
+           a = expense_record()
+           total_income = a
         elif(user==3):
-            print(total_income) 
+            print(f"Your wallet after all transaction: {total_income}")
+        elif(user==4):
+            record()
         elif(user==5):
             break       
 show_menu()
